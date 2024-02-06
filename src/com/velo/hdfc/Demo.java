2520ad7acf330c7ec3746eb94b3ec272b5853173
Namrata <nammondhe@gmail.com>
2024-02-06 12:00:28 +0530

    added code for arraylist

---
+++ src/com/velo/hdfc/Demo.java
@@ -0,0 +1,17 @@
+package com.velo.hdfc;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList al= new ArrayList();
+al.add(25);
+al.add("hello");
+al.add(null);
+al.add(null);
+System.out.println(al);
+	}
+
+}
